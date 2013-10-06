0c722d6ce055a38ad9d4a17976a014e687129328
winebarrel <sgwr_dts@yahoo.co.jp>
2013-10-06 14:00:19 +0900

    add health-check

---
+++ lib/kelbim/dsl/health-check.rb
@@ -0,0 +1,55 @@
+require 'ostruct'
+require 'kelbim/dsl/checker'
+
+module Kelbim
+  class DSL
+    class EC2
+      class LoadBalancer
+        class HealthCheck
+          def initialize(load_balancer, protocol_prots, &block)
+            @error_identifier = "LoadBalancer `#{load_balancer}`"
+            @result = {}
+            instance_eval(&block)
+          end
+
+          def result
+            [:target, :timeout, :interval, :healthy_threshold, :unhealthy_threshold].each do |name|
+              required(name, @result[name])
+            end
+
+            @result
+          end
+
+          def target(value)
+            call_once(:target)
+            @result.target = value
+          end
+
+          def timeout(value)
+            call_once(:timeout)
+            expected_type(value, Integer)
+            @result.timeout = value
+          end
+
+          def interval(value)
+            call_once(:interval)
+            expected_type(value, Integer)
+            @result.interval = value
+          end
+
+          def healthy_threshold(value)
+            call_once(:healthy_threshold)
+            expected_type(value, Integer)
+            @result.healthy_threshold = value
+          end
+
+          def unhealthy_threshold(value)
+            call_once(:unhealthy_threshold)
+            expected_type(value, Integer)
+            @result.unhealthy_threshold = value
+          end
+        end # HealthCheck
+      end # LoadBalancer
+    end # EC2
+  end # DSL
+end # Kelbim
