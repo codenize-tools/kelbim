71deaa6827de2bb8f58f622c260070269608776c
winebarrel <sgwr_dts@yahoo.co.jp>
2013-10-06 13:31:29 +0900

    fix checker

--- lib/kelbim/dsl/checker.rb
+++ lib/kelbim/dsl/checker.rb
@@ -2,6 +2,29 @@ module Kelbim
   class DSL
     module Checker
       private
+      def required(name, value)
+        invalid = false
+
+        if value
+          case value
+          when Strin
+            invalid = value.strip.empty?
+          when Array, Hash
+            invalid = value.empty?
+          end
+        else
+          invalid = true
+        end
+
+        raise __identify("`#{name}` is required:") if invalid
+      end
+
+      def expected_type(value, *types)
+        unless types.any? {|t| value.kind_of?(t) }
+          raise __identify("Invalid type: #{t}")
+        end
+      end
+
       def call_once(method_name)
         @called ||= []
 
