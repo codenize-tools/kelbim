8c6c0845cd6e86d8b549855ab51f3da8928a78ba
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-10-27 13:17:59 +0900

    add logger

---
+++ lib/kelbim/logger.rb
@@ -0,0 +1,33 @@
+require 'logger'
+require 'singleton'
+require 'kelbim/ext/string-ext'
+
+module Kelbim
+  class Logger < ::Logger
+    include Singleton
+
+    def initialize
+      super($stdout)
+
+      self.formatter = proc do |severity, datetime, progname, msg|
+        "#{msg}\n"
+      end
+
+      self.level = Logger::INFO
+    end
+
+    def set_debug(value)
+      self.level = value ? Logger::DEBUG : Logger::INFO
+    end
+
+    module ClientHelper
+      def log(level, message, color, log_id = nil)
+        message = "[#{level.to_s.upcase}] #{message}" unless level == :info
+        message << ": #{log_id}" if log_id
+        message << ' (dry-run)' if @options && @options.dry_run
+        logger = (@options && @options.logger) || Kelbim::Logger.instance
+        logger.send(level, message.send(color))
+      end
+    end # ClientHelper
+  end # Logger
+end # Piculet
