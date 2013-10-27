2cbf0fc3c5747611b5c856caac5e3eda0fcbc127
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-10-27 22:51:34 +0900

    add policy wrapper

---
+++ lib/kelbim/wrapper/policy.rb
@@ -0,0 +1,51 @@
+require 'forwardable'
+require 'kelbim/policy-types'
+require 'kelbim/logger'
+
+module Kelbim
+  class ELBWrapper
+    class LoadBalancerCollection
+      class LoadBalancer
+        class ListenerCollection
+          class Listener
+            class PolicyCollection
+              class Policy
+                extend Forwardable
+                include Logger::ClientHelper
+
+                def_delegators(
+                  :@policy,
+                  :type)
+
+                def initialize(policy, policy_collection, options)
+                  @policy = policy
+                  @policy_collection = policy_collection
+                  @options = options
+                end
+
+                def eql?(dsl)
+                  dsl_type, dsl_name_or_attrs = dsl
+
+                  if Kelbim::PolicyTypes.name?(dsl_name_or_attrs)
+                    @policy.name == dsl_name_or_attrs
+                  else
+                    aws_attrs = Kelbim::PolicyTypes.expand(@policy.type, @policy.attributes)
+                    aws_attrs.sort == dsl_name_or_attrs.sort
+                  end
+                end
+
+                def update(dsl)
+                  # XXX:
+                end
+
+                def delete
+                  # XXX:
+                end
+              end # Policy
+            end # PolicyCollection
+          end # Listener
+        end # ListenerCollection
+      end # LoadBalancer
+    end # LoadBalancerCollection
+  end # ELBWrapper
+end # Kelbim
