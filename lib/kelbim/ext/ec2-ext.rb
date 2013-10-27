3fb6fa46554531cdb05105b67b3f5c8cbc40f883
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-10-27 13:18:38 +0900

    fix ec2-ext

--- lib/kelbim/ext/ec2-ext.rb
+++ lib/kelbim/ext/ec2-ext.rb
@@ -3,13 +3,25 @@ require 'aws-sdk'
 module AWS
   class EC2
     def instance_names
-      id_names = {}
+      vpc_instance_id_names = {}
 
       self.instances.each do |i|
-        id_names[i.id] = i.tags['Name']
+        vpc_instance_id_names[i.vpc_id] ||= {}
+        vpc_instance_id_names[i.vpc_id][i.id] = i.tags['Name']
       end
 
-      return id_names
+      return vpc_instance_id_names
+    end
+
+    def security_group_names
+      vpc_sg_id_names = {}
+
+      self.security_groups.each do |i|
+        vpc_sg_id_names[i.vpc_id] ||= {}
+        vpc_sg_id_names[i.vpc_id][i.id] = i.name
+      end
+
+      return vpc_sg_id_names
     end
   end
 end # AWS
