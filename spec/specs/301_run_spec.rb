de45c26ff07cbc793624432924af35316f54e5a7
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-04 22:44:34 +0900

    add testcase

---
+++ spec/specs/301_run_spec.rb
@@ -0,0 +1,117 @@
+require File.expand_path("#{File.dirname __FILE__}/../spec_config")
+
+describe Kelbim::Client do
+  it do
+    elbfile(:run_spec => true) { (<<-EOS)
+ec2 do
+  load_balancer "my-load-balancer" do
+    spec do
+      expect(1).to eq(1)
+    end
+
+    instances(
+      "cthulhu",
+      "hastur",
+      "nyar",
+      "yog"
+    )
+
+    listeners do
+      listener [:http, 80] => [:http, 80]
+    end
+
+    health_check do
+      target "HTTP:80/index.html"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    availability_zones(
+      "us-west-1a",
+      "us-west-1b",
+      "us-west-1c"
+    )
+  end
+end
+
+ec2 "vpc-c1cbc2a3" do
+  load_balancer "my-load-balancer-1" do
+    spec do
+      expect(1).to eq(1)
+    end
+
+    instances(
+      "cthulhu",
+      "hastur",
+      "nyar",
+      "yog"
+    )
+
+    listeners do
+      listener [:tcp, 8080] => [:tcp, 8080]
+    end
+
+    health_check do
+      target "TCP:8080"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    subnets(
+      "subnet-5e1c153c",
+      "subnet-567c3610",
+    )
+
+    security_groups(
+      "default",
+      "vpc-c1cbc2a3-1",
+      "vpc-c1cbc2a3-2"
+    )
+  end
+end
+
+ec2 "vpc-cbcbc2a9" do
+  load_balancer "my-load-balancer-2", :internal => true do
+    spec do
+      expect(1).to eq(1)
+    end
+
+    instances(
+      "cthulhu",
+      "hastur",
+      "nyar",
+      "yog"
+    )
+
+    listeners do
+      listener [:http, 80] => [:http, 80]
+    end
+
+    health_check do
+      target "HTTP:80/index.html"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    subnets(
+      "subnet-211c1543",
+      "subnet-487c360e",
+    )
+
+    security_groups(
+      "default",
+      "vpc-cbcbc2a9-1",
+      "vpc-cbcbc2a9-2"
+    )
+  end
+end
+      EOS
+    }
+  end
+end
