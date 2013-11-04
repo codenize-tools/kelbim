e5eff30c345ee98cbc926202cfb86f70bcdd2cbe
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-04 22:02:20 +0900

    add testcase

---
+++ spec/specs/202_delete_all.rb
@@ -0,0 +1,142 @@
+require File.expand_path("#{File.dirname __FILE__}/../spec_config")
+
+describe Kelbim::Client do
+  before do
+    elbfile { (<<-EOS)
+ec2 do
+  load_balancer "my-load-balancer" do
+    instances(
+      "cthulhu",
+      "hastur",
+      "nyar",
+      "yog"
+    )
+
+    listeners do
+      listener [:http, 80] => [:http, 80] do
+        lb_cookie_stickiness "CookieExpirationPeriod"=>"10"
+      end
+      listener [:https, 443] => [:http, 80] do
+        app_cookie_stickiness "CookieName"=>"20"
+        ssl_negotiation ["Protocol-TLSv1", "Protocol-SSLv3", "AES256-SHA", "DES-CBC3-SHA", "AES128-SHA", "RC4-SHA", "RC4-MD5"]
+        server_certificate "test1"
+      end
+    end
+
+    health_check do
+      target "HTTP:80/index.html"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    availability_zones(
+      "us-west-1a",
+      "us-west-1b",
+      "us-west-1c"
+    )
+  end
+end
+
+ec2 "vpc-c1cbc2a3" do
+  load_balancer "my-load-balancer-1" do
+    instances(
+      "cthulhu",
+      "hastur",
+      "nyar",
+      "yog"
+    )
+
+    listeners do
+      listener [:tcp, 8080] => [:tcp, 8080]
+      listener [:https, 443] => [:http, 80] do
+        lb_cookie_stickiness "CookieExpirationPeriod"=>"11"
+        ssl_negotiation ["Protocol-TLSv1", "Protocol-SSLv3", "AES256-SHA", "DES-CBC3-SHA", "AES128-SHA", "RC4-SHA", "RC4-MD5"]
+        server_certificate "test2"
+      end
+    end
+
+    health_check do
+      target "TCP:8080"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    subnets(
+      "subnet-5e1c153c",
+      "subnet-567c3610",
+    )
+
+    security_groups(
+      "default",
+      "vpc-c1cbc2a3-1",
+      "vpc-c1cbc2a3-2"
+    )
+  end
+end
+
+ec2 "vpc-cbcbc2a9" do
+  load_balancer "my-load-balancer-2", :internal => true do
+    instances(
+      "cthulhu",
+      "hastur",
+      "nyar",
+      "yog"
+    )
+
+    listeners do
+      listener [:http, 80] => [:http, 80] do
+        lb_cookie_stickiness "CookieExpirationPeriod"=>"12"
+      end
+      listener [:https, 443] => [:http, 80] do
+        app_cookie_stickiness "CookieName"=>"22"
+        ssl_negotiation ["Protocol-TLSv1", "Protocol-SSLv3", "AES256-SHA", "DES-CBC3-SHA", "AES128-SHA", "RC4-SHA", "RC4-MD5"]
+        server_certificate "test3"
+      end
+    end
+
+    health_check do
+      target "HTTP:80/index.html"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    subnets(
+      "subnet-211c1543",
+      "subnet-487c360e",
+    )
+
+    security_groups(
+      "default",
+      "vpc-cbcbc2a9-1",
+      "vpc-cbcbc2a9-2"
+    )
+  end
+end
+      EOS
+    }
+  end
+
+  it do
+    updated = elbfile { (<<-EOS)
+ec2 do
+end
+
+ec2 "vpc-c1cbc2a3" do
+end
+
+ec2 "vpc-cbcbc2a9" do
+end
+      EOS
+    }
+
+    expect(updated).to be_true
+    exported = export_elb
+    expect(exported).to eq({})
+  end
+end
