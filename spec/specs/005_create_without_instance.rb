0e5354b22cef02655ce24f2edd0cc924f465e306
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-04 15:02:17 +0900

    add testcase

---
+++ spec/specs/005_create_without_instance.rb
@@ -0,0 +1,152 @@
+ENV['IGNORE_RECURSION'] = '1'
+require File.expand_path("#{File.dirname(__FILE__)}/../kelbim_spec.rb")
+
+describe Kelbim::Client do
+  it do
+    updated = elbfile { (<<-EOS)
+ec2 do
+  load_balancer "my-load-balancer" do
+    instances(
+
+    )
+
+    listeners do
+      listener [:http, 80] => [:http, 80]
+    end
+
+    health_check do
+      target "HTTP:80/index.html"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    availability_zones(
+      "us-west-1a",
+    )
+  end
+end
+
+ec2 "vpc-c1cbc2a3" do
+  load_balancer "my-load-balancer-1" do
+    instances(
+    )
+
+    listeners do
+      listener [:tcp, 8080] => [:tcp, 8080]
+    end
+
+    health_check do
+      target "TCP:8080"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    subnets(
+      "subnet-5e1c153c",
+    )
+
+    security_groups(
+      "default",
+    )
+  end
+end
+
+ec2 "vpc-cbcbc2a9" do
+  load_balancer "my-load-balancer-2", :internal => true do
+    instances(
+    )
+
+    listeners do
+      listener [:http, 80] => [:http, 80]
+    end
+
+    health_check do
+      target "HTTP:80/index.html"
+      timeout 5
+      interval 30
+      healthy_threshold 10
+      unhealthy_threshold 2
+    end
+
+    subnets(
+      "subnet-211c1543",
+    )
+
+    security_groups(
+      "default",
+    )
+  end
+end
+      EOS
+    }
+
+    expect(updated).to be_true
+    exported = export_elb
+    expect(exported).to eq(
+{nil=>
+  {"my-load-balancer"=>
+    {:instances=>[],
+     :listeners=>
+      [{:protocol=>:http,
+        :port=>80,
+        :instance_protocol=>:http,
+        :instance_port=>80,
+        :server_certificate=>nil,
+        :policies=>[]}],
+     :health_check=>
+      {:interval=>30,
+       :target=>"HTTP:80/index.html",
+       :healthy_threshold=>10,
+       :timeout=>5,
+       :unhealthy_threshold=>2},
+     :scheme=>"internet-facing",
+     :dns_name=>"my-load-balancer-NNNNNNNNNN.us-west-1.elb.amazonaws.com",
+     :availability_zones=>["us-west-1a"]}},
+ "vpc-c1cbc2a3"=>
+  {"my-load-balancer-1"=>
+    {:instances=>[],
+     :listeners=>
+      [{:protocol=>:tcp,
+        :port=>8080,
+        :instance_protocol=>:tcp,
+        :instance_port=>8080,
+        :server_certificate=>nil,
+        :policies=>[]}],
+     :health_check=>
+      {:interval=>30,
+       :target=>"TCP:8080",
+       :healthy_threshold=>10,
+       :timeout=>5,
+       :unhealthy_threshold=>2},
+     :scheme=>"internet-facing",
+     :dns_name=>"my-load-balancer-1-NNNNNNNNNN.us-west-1.elb.amazonaws.com",
+     :subnets=>["subnet-5e1c153c"],
+     :security_groups=>["default"]}},
+ "vpc-cbcbc2a9"=>
+  {"my-load-balancer-2"=>
+    {:instances=>[],
+     :listeners=>
+      [{:protocol=>:http,
+        :port=>80,
+        :instance_protocol=>:http,
+        :instance_port=>80,
+        :server_certificate=>nil,
+        :policies=>[]}],
+     :health_check=>
+      {:interval=>30,
+       :target=>"HTTP:80/index.html",
+       :healthy_threshold=>10,
+       :timeout=>5,
+       :unhealthy_threshold=>2},
+     :scheme=>"internal",
+     :dns_name=>
+      "internal-my-load-balancer-2-NNNNNNNNNN.us-west-1.elb.amazonaws.com",
+     :subnets=>["subnet-211c1543"],
+     :security_groups=>["default"]}}}
+    )
+  end
+end
