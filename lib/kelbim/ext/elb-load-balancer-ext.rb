cf1db07d417ef04463886e16de1d33e6a562363b
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-08 22:38:58 +0900

    add attributes medhotd to LoadBalancer

--- lib/kelbim/ext/elb-load-balancer-ext.rb
+++ lib/kelbim/ext/elb-load-balancer-ext.rb
@@ -10,6 +10,24 @@ module AWS
           lb[:load_balancer_name] == name
         end
       end
+
+      def attributes
+        unless @attributes
+          credentials = AWS.config.credential_provider.credentials
+          elb = AWS::ELB.new(credentials)
+          @attributes = elb.client.describe_load_balancer_attributes(
+            :load_balancer_name => self.name)
+        end
+
+        return @attributes.dup
+      end
+
+      def attributes=(attrs)
+        credentials = AWS.config.credential_provider.credentials
+        elb = AWS::ELB.new(credentials)
+        elb.client.modify_load_balancer_attributes(attrs)
+        (@attributes = attrs).dup
+      end
     end # LoadBalancer
   end # ELB
 end # AWS
